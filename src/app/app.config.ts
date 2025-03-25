51c10dee0b88752a275511f1f669d0ad41b75275
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-25 20:51:06 +0200

    Provide HTTP Client With Fetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig , provideZoneChangeDetection } from '@angular/core';
 import { provideRouter , withHashLocation , withInMemoryScrolling , withViewTransitions } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration , withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideHttpClient , withFetch } from '@angular/common/http';
 
 export const appConfig : ApplicationConfig = {
   providers : [
@@ -27,6 +27,7 @@ export const appConfig : ApplicationConfig = {
           darkModeSelector : '.my-app-dark'
         }
       }
-    } )
+    } ) ,
+    provideHttpClient ( withFetch () )
   ]
 };
