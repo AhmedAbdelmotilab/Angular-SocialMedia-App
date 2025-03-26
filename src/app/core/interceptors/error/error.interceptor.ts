ca6e7fd2a7fb42402920829877dfa6df1a8a5082
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-26 06:55:10 +0200

    Update Display Error From error.interceptor.ts

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -6,7 +6,7 @@ import { inject } from '@angular/core';
 export const errorInterceptor : HttpInterceptorFn = ( req , next ) => {
   const messageService = inject ( MessageService );
   return next ( req ).pipe ( catchError ( ( err ) => {
-    messageService.add ( { severity : 'error' , summary : 'Error' , detail : err.error.message , life : 3000 } );
+    messageService.add ( { severity : 'error' , summary : 'Error' , detail : err.error.error , life : 3000 } );
     return throwError ( () => {
       return err;
     } );
