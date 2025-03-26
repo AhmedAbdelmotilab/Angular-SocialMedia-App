9e433339c1e9d1852acd016c815b375af615cbea
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-26 06:20:56 +0200

    Using NG Prime Toaster For Alerts Error Toaster Inside The error.interceptor.ts

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -1,9 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError , throwError } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { inject } from '@angular/core';
 
 export const errorInterceptor : HttpInterceptorFn = ( req , next ) => {
+  const messageService = inject ( MessageService );
   return next ( req ).pipe ( catchError ( ( err ) => {
-    console.log ( err );
+    messageService.add ( { severity : 'error' , summary : 'Error' , detail : err.error.message , life : 3000 } );
     return throwError ( () => {
       return err;
     } );
