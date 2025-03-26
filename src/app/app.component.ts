5535f7084ef52f8467e794091c23dda02df4199e
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-26 02:45:57 +0200

    Install Flowbite For Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { afterNextRender , Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+@Component ( {
+  selector : 'app-root' ,
+  imports : [ RouterOutlet ] ,
+  templateUrl : './app.component.html' ,
+  styleUrl : './app.component.scss'
+} )
 export class AppComponent {
   title = 'SocailMediaApp';
+
+  constructor () {
+    afterNextRender ( () => {
+      initFlowbite ();
+
+    } );
+  }
 }
